cebbbd35b881955dc5e9f933272327829ce5a874
Lasha Jini <lasha.jini@gmail.com>
2025-02-04 10:42:32 +0000

    chore: expand env variables

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
+
+// Load and expand environment variables
+dotenvExpand.expand(dotenv.config());
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
