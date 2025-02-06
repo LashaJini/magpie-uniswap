bf900b1b4d36e18a6cf1cc97cdbdc4f26264f694
Lasha Jini <lasha.jini@gmail.com>
2025-02-06 12:11:12 +0000

    chore: re-export dtos

---
+++ src/storage/database/dtos/index.ts
@@ -0,0 +1,6 @@
+export { PoolTickInsertDTO } from './pool-tick-insert.dto'
+export { PoolUpsertDTO } from './pool-upsert.dto'
+export { PoolWithTicksAndTokensDTO } from './pool-with-ticks-and-tokens.dto'
+export { TickUpsertDTO } from './tick-upsert.dto'
+export { TokenInsertDTO } from './token-insert.dto'
+
